5231943e139827023e333298dca0d5d58b287f01
Matheus <matheusferreirasjc@gmail.com>
2025-10-22 22:47:16 -0300

    feat: Adiciona Mapper do Category e usa ResponseEntity no controller

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -3,9 +3,14 @@ package br.com.movieflix.controller;
 import br.com.movieflix.controller.request.CategoryRequest;
 import br.com.movieflix.controller.response.CategoryResponse;
 import br.com.movieflix.entity.Category;
+import br.com.movieflix.mapper.CategoryMapper;
 import br.com.movieflix.repository.CategoryRepository;
 import br.com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,27 +26,34 @@ public class CategoryController {
 
     // Listar todas as categorias
     @GetMapping
-    public List<CategoryResponse> getAllCategories() {
-        return categoryService.findAll();
+    public ResponseEntity<List<CategoryResponse>> getAllCategories() {
+        List<Category> categories = categoryService.findAll();
+        List<CategoryResponse> list = categories.stream()
+                .map(CategoryMapper::toCategoryResponse)
+                .toList();
+
+
+        return ResponseEntity.ok(list);
     }
 
 
     @PostMapping
-    public CategoryResponse saveCategory(@RequestBody CategoryRequest request){
-        return categoryService.saveCategory(category);
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request){
+        Category newCategory = CategoryMapper.toCategory(request);
+        Category savedCategory = categoryService.saveCategory(newCategory);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategory));
     }
 
     @GetMapping("/{id}")
-    public CategoryResponse getByCategoryId(@PathVariable Long id){
-        Optional<Category> optCategory = categoryService.findById(id);
-        if(optCategory.isPresent()){
-            return optCategory.get();
-        }
-        return null;
+    public ResponseEntity<CategoryResponse> getByCategoryId(@PathVariable Long id) {
+        return categoryService.findById(id)
+                .map(category -> ResponseEntity.ok().body(CategoryMapper.toCategoryResponse(category)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategoryById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteCategoryById(@PathVariable Long id){
         categoryService.deleteCategory(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/br/com/movieflix/entity/Category.java
+++ src/main/java/br/com/movieflix/entity/Category.java
@@ -1,11 +1,11 @@
 package br.com.movieflix.entity;
 
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
