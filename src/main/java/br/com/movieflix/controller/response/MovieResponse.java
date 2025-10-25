52a696da7cc976f546ef39588401339b57302b25
Matheus <matheusferreirasjc@gmail.com>
2025-10-24 22:22:10 -0300

    refactor: Refatora o formato das datas para o modelo brasileiro, dia/mes/ano

--- src/main/java/br/com/movieflix/controller/response/MovieResponse.java
+++ src/main/java/br/com/movieflix/controller/response/MovieResponse.java
@@ -1,5 +1,6 @@
 package br.com.movieflix.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ import java.util.List;
 public record MovieResponse(Long id,
                             String title,
                             String description,
+                            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
                             LocalDate releaseDate,
                             double rating,
                             List<CategoryResponse> categories,
