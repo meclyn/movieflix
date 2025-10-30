7749bd0312a20b1d4b59262299a672bc63e4614b
Matheus <matheusferreirasjc@gmail.com>
2025-10-29 22:13:13 -0300

    feat: Adiciona a rota para registro de usuario usando ResponseEntity

--- src/main/java/br/com/movieflix/controller/AuthController.java
+++ src/main/java/br/com/movieflix/controller/AuthController.java
@@ -1,4 +1,25 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.controller.request.UserRequest;
+import br.com.movieflix.controller.response.UserResponse;
+import br.com.movieflix.entity.User;
+import br.com.movieflix.mapper.UserMapper;
+import br.com.movieflix.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/movieflix/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest userRequest){
+        User savedUser = userService.save(UserMapper.toUser(userRequest));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+    }
 }
